5123b38a8016bb3bde07910515a9df59f4681847
adityaghai07 <119144814+adityaghai07@users.noreply.github.com>
2023-10-01 21:50:05 +0530

    Update main.py
    added some required comments

--- SureBot/main.py
+++ SureBot/main.py
@@ -13,7 +13,7 @@ from langchain.vectorstores import FAISS
 
 from dotenv import load_dotenv
 
-load_dotenv()  # take environment variables from .env.
+load_dotenv()  # take environment variables from .env.take the api key .
 
 st.title("SureBot")
 st.sidebar.title("News Article URLs")
@@ -32,19 +32,19 @@ llm = OpenAI(temperature=0.9, max_tokens=500)
 if process_url_clicked:
     # load data
     loader = UnstructuredURLLoader(urls=urls)
-    main_placeholder.text("Data Loading...Started...✅✅✅")
+    main_placeholder.text("Data Loading...Started...")
     data = loader.load()
     # split data
     text_splitter = RecursiveCharacterTextSplitter(
         separators=['\n\n', '\n', '.', ','],
         chunk_size=1000
     )
-    main_placeholder.text("Text Splitter...Started...✅✅✅")
+    main_placeholder.text("Text Splitter...Started...")
     docs = text_splitter.split_documents(data)
     # create embeddings and save it to FAISS index
     embeddings = OpenAIEmbeddings()
     vectorstore_openai = FAISS.from_documents(docs, embeddings)
-    main_placeholder.text("Embedding Vector Started Building...✅✅✅")
+    main_placeholder.text("Embedding Vector Started Building...")
     time.sleep(2)
 
     # Save the FAISS index to a pickle file
@@ -58,14 +58,14 @@ if query:
             vectorstore = pickle.load(f)
             chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=vectorstore.as_retriever())
             result = chain({"question": query}, return_only_outputs=True)
-            # result will be a dictionary of this format --> {"answer": "", "sources": [] }
+            # result will be a dictionary 
             st.header("Answer")
             st.write(result["answer"])
 
-            # Display sources, if available
+            # Display sources
             sources = result.get("sources", "")
             if sources:
                 st.subheader("Sources:")
-                sources_list = sources.split("\n")  # Split the sources by newline
+                sources_list = sources.split("\n")  # Split by newline
                 for source in sources_list:
                     st.write(source)
